b2326124046a2459cf10c9ca5aa3b3c479ca4a88
Daniel Losey <daniellosey@Daniels-MacBook-Pro.local>
2024-02-24 10:41:58 -0600

    right and left freezable

--- layers.py
+++ layers.py
@@ -5,7 +5,8 @@ from typing import Union
 
 
 class RangeToken(Individual):
-    def __init__(self, gene_pool: genepools.GenePool, start: int, end: int, source: str):
+    def __init__(self, gene_pool: genepools.GenePool, start: int, end: int, source: str, left_freezable=False,
+                 right_freezable=False):
         super().__init__(gene_pool=gene_pool, genes=None, fitness=1.0)
         self.start = start
         self.end = end
@@ -15,6 +16,9 @@ class RangeToken(Individual):
         self.length = len(self.source)
         self.right_frozen = False
         self.left_frozen = False
+        self.right_freezable = right_freezable
+        self.left_freezable = left_freezable
+
         try:
             while self.token[0] == " ":
                 self.start += 1
@@ -22,41 +26,61 @@ class RangeToken(Individual):
                 self.left_frozen = True
         except IndexError:
             pass
+
     def copy(self):
         return RangeToken(self.gene_pool, self.start, self.end, self.source)
+
     def mutate(self):
-        choice = random.choice(['shift', 'length'])
-        # if self.age > 10:
-        #     self.redo()
+        left_mutation = random.choice([1, 0, -1])
+        right_mutation = random.choice([1, 0, -1])
 
-        self.age += 1
-        if choice == 'shift':
-            if self.end < self.length + 1:
-                shift = random.choice([1, -1])
-                if not self.left_frozen:
-                    self.start += shift
-                self.end += shift
-        if choice == "length":
-            if self.end < self.length + 1:
-                position = random.choice(['start', 'end'])
-                if position == 'start' and not self.left_frozen:
-                    self.start += random.choice([-1, 1])
-                if position == 'end' or self.left_frozen:
-                    self.end += random.choice([-1, 1])
+        if not self.left_frozen and left_mutation == 1:
+            self.start += left_mutation
 
-        if self.end > self.start:
-            self.end, self.start = self.start, self.end
+        if not self.right_frozen and right_mutation == -1:
+            self.right += rigth_mutation
+
+        if self.left_frozen and left_mutation == 1:
+            self.start += left_mutation
+            self.left_frozen = False
+
+        if self.right_frozen and right_mutation == -1:
+            self.right += rigth_mutation
+            self.right_frozen = False
 
         self.token = self.source[self.start:self.end]
-        try:
-            if self.token[0] == " ":
+
+        if self.token == '':
+            self.redo()
+
+        if self.right_freezable and self.left_freezable:
+            while " " in self.token:
+                self.end -= 1
+                self.start -= 2
+                if self.end > self.start:
+                    self.end, self.start = self.start, self.end
+                self.token = self.source[self.start:self.end]
+            self.left_frozen = False
+            self.right_frozen = False
+
+        elif self.left_freezable:
+            while token[0] == " ":
                 self.start += 1
-                self.token = self.token[1:-1]
+                if self.start == self.end:
+                    self.end += 1
+                self.token = self.source[self.start:self.end]
                 self.left_frozen = True
-        except IndexError:
-            pass
-        # if self.token[-1] == " ":
-        #     self.right_frozen True
+
+        elif self.right_freezable:
+            while token[-1] == " ":
+                self.end -= 1
+                if self.start == self.end:
+                    self.end -= 1
+                self.token = self.source[self.start:self.end]
+                self.right_frozen = True
+
+        self.end, self.start = self.start, self.end
+
     def redo(self):
         new = self.gene_pool.generate_individual()
         self.start = new.start
@@ -65,17 +89,6 @@ class RangeToken(Individual):
         self.left_frozen = new.left_frozen
         self.right_frozen = new.left_frozen
         self.age = 0
-
-        try:
-            while self.token[0] == " ":
-                self.start += 1
-                self.token = self.token[1:-1]
-                self.left_frozen = True
-        except IndexError:
-            pass
-
-
-
 
 
 def create_rangetoken(token: str, source_text: str, gene_pool: genepools.GenePool) -> Union[RangeToken, None]:
@@ -86,6 +99,8 @@ def create_rangetoken(token: str, source_text: str, gene_pool: genepools.GenePoo
         return token
     except ValueError:
         return None
+
+
 class MutateToken(layers.layer.Mutate):
     def __init__(self, individual_selection, refit: bool = True):
         super().__init__(individual_selection=individual_selection, refit=refit, gene_selection=None)
